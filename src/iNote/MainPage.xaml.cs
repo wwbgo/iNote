62c5d32c7452384043f8bf9319deff8204377fae
wwb <wwbgo@qq.com>
2022-08-22 14:23:09 +0800

    add topmost for windows

--- src/iNote/MainPage.xaml.cs
+++ src/iNote/MainPage.xaml.cs
@@ -1,24 +1,58 @@
-﻿namespace iNote;
+﻿using Microsoft.Maui.Controls;
+using Vanara.PInvoke;
+using static Vanara.PInvoke.User32;
+
+namespace iNote;
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
+    bool _isTopMost = false;
 
-	public MainPage()
-	{
-		InitializeComponent();
-	}
+    public MainPage()
+    {
+        InitializeComponent();
+    }
 
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
+    private async void ForegroundBtn_Clicked(object sender, EventArgs e)
+    {
+        var hWnd = GetActiveWindow();
+        if (!hWnd.IsNull)
+        {
+            await SetForegroundWindowCustom(hWnd);
+        }
+    }
+    private async Task SetForegroundWindowCustom(HWND hwnd)
+    {
+        noteEditor.Text += $"{DateTime.Now}{Environment.NewLine}";
 
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
+        if (_isTopMost)
+        {
+            var result = SetWindowPos(hwnd, HWND.HWND_NOTOPMOST, 0, 0, 0, 0, SetWindowPosFlags.SWP_NOSIZE | SetWindowPosFlags.SWP_NOMOVE);
+            if (result)
+            {
+                _isTopMost = !_isTopMost;
+                ForegroundBtn.Text = "置顶";
+            }
+            else
+            {
+                await DisplayAlert("警告", "取消置顶失败", "确定");
+            }
+        }
+        else
+        {
+            ForegroundBtn.Text = "置顶中...";
 
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
+            var result = SetWindowPos(hwnd, HWND.HWND_TOPMOST, 0, 0, 0, 0, SetWindowPosFlags.SWP_NOSIZE | SetWindowPosFlags.SWP_NOMOVE);
+            if (result)
+            {
+                _isTopMost = !_isTopMost;
+                ForegroundBtn.Text = "取消置顶";
+            }
+            else
+            {
+                await DisplayAlert("警告", "置顶失败", "确定");
+            }
+        }
+    }
 }
 
